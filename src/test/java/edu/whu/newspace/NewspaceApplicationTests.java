194f634fd0c412d9c95f75d4ac4e399bfcf0854c
Akihabara <2607038766@qq.com>
2023-05-23 09:53:37 +0800

    项目初始化

---
+++ src/test/java/edu/whu/newspace/NewspaceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.whu.newspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewspaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
